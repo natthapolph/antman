b07f1af87735dcea56d7da538f50b2e24e1e9841
TiTle-GS <tle19111@gmail.com>
2020-07-30 21:51:06 +0700

    css login

--- src/components/section/Login.js
+++ src/components/section/Login.js
@@ -2,12 +2,22 @@ import React from "react";
 
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-      <input type="email" placeholder="email@" />
-      <input type="password" placeholder="password" />
-      <button>login</button>
+    <div className="Body-login">
+      <div className="Container-login">
+         <h2>Login</h2>
+        <input type="email" placeholder="Enter Your Email" />
+        <input type="password" placeholder="Enter Your Password" />
+        <div className="Login-row">
+          <label className="Label-login">
+            <input type="checkbox" />
+            Remember Password
+          </label>
+          <a href="#">Forget Password?</a>
+        </div>
+        <button className="Button-login">login</button>
+      </div>
     </div>
+ 
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './components/section/Login.css'
+import './components/section/Register.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
