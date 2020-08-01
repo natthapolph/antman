5c2b0814b6e2741de5972b5cb328c888b3cca2b3
natthapolph <birdazaz123@gmail.com>
2020-08-01 22:23:23 +0700

    firebase-updatelogin-birdbird

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-
+import auth from "./section/firebase/index";
 function Header() {
+  const [status, setStatus] = useState(false);
+  const [email, setEmail] = useState("");
+  useEffect(() => {
+    const handleAuth = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setStatus(true);
+        setEmail(user.email);
+      } else {
+      }
+    });
+
+    return () => {
+      handleAuth();
+    };
+  }, []);
+
+  const handleLogout = () => {
+    auth.signOut().then((response) => {
+      setStatus(false);
+      console.log(response);
+    });
+  };
   const [toggle, setToggle] = useState(false);
 
   const menuToggle = () => {
@@ -15,21 +37,38 @@ function Header() {
       </div>
       <>
         <ul className={toggle ? "toggle" : ""}>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/blog">Blog</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
+          {!status && (
+            <>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </>
+          )}
+          {status && (
+            <>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/blog">Blog</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <h1>{email}</h1>
+              </li>
+              <li>
+                <Link to="#" onClick={handleLogout}>
+                  Logout
+                </Link>
+                {/* <h1 onClick={handleLogout}>logout</h1> */}
+              </li>
+            </>
+          )}
           <li className="close" onClick={menuToggle}>
             X
           </li>
